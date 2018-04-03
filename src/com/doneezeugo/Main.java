d61e68c94fef91feb2299cb0b18c19e5e82dee8f
deezeesms <deezeesms@gmail.com>
2018-04-03 00:20:29 -0400

    Making it 25

--- src/com/doneezeugo/Main.java
+++ src/com/doneezeugo/Main.java
@@ -3,7 +3,7 @@ package com.doneezeugo;
 public class Main{
 
     public static void main(String[] args) {
-        for (int i = 1; i < 12; i++) {
+        for (int i = 1; i < 25; i++) {
             System.out.println("from IDE-IntelliJ ... " + i);
         }
 
